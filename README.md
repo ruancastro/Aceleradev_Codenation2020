1ab18c946ebb1f2093a681ad038baa2e02c3e445
ruancastro <ruancastro15@gmail.com>
2020-05-23 17:42:24 -0300

    escopo basico

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Aceleradev_Codenation2020
+## link do desafio : https://codenation.dev/aceleradev/ds-online-2/challenge/enem-ps
---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+# df_train = pd.read_csv("/home/ruan/Área de Trabalho/codes/CodeNation/Aceleradev_Codenation2020/input/train.csv")
+# print(df_train.head())
+# # print(df_train.describe())
+# print(df_train.columns)
+# # print(df_train.index()
+# print(df_train.shape)
+
+df_dicionario = pd.read_excel("/home/ruan/Área de Trabalho/codes/CodeNation/Aceleradev_Codenation2020/input/Dicionario_Microdados_Enem_2016.xlsx")
+print(df_dicionario.head())
+# print(df_dicionario.describe())
+print(df_dicionario.columns)
+# print(df_dicionario.index()
+print(df_dicionario.shape)
